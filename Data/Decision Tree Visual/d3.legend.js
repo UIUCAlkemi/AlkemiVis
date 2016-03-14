12f1882c7ee1017c0e6403bd2529616b0c195188
Haoming <hlai10@illinois.edu>
2016-03-14 15:23:57 -0500

    legend done
    may need to be revised

--- Data/Decision Tree Visual/d3.legend.js
+++ Data/Decision Tree Visual/d3.legend.js
@@ -12,7 +12,7 @@ d3.legend = function(g) {
         lb = g.selectAll(".legend-box").data([true]),
         li = g.selectAll(".legend-items").data([true])
 
-    lb.enter().append("rect").classed("legend-box",true)
+ //   lb.enter().append("rect").classed("legend-box",true)
     li.enter().append("g").classed("legend-items",true)
 
     svg.selectAll("[data-legend]").each(function() {
